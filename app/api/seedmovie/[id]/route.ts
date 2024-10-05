95a6bbf5f9333777288a9d55f70f08889ea61963
tomas <josetomashervas@gmail.com>
2024-10-05 15:03:21 +0200

    delete movie

--- app/api/seedmovie/[id]/route.ts
+++ app/api/seedmovie/[id]/route.ts
@@ -32,3 +32,31 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
         return new NextResponse("Error" + error, { status: 500 })
     }
 }
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const token = req.headers.get('Authorization')?.split(' ')[1];
+    const decoded = verifyToken(token || '');
+
+    // Verificar si el usuario tiene privilegios de administrador
+    if ((decoded as JwtPayload).user !== process.env.ADMIN) {
+        return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const id = +params.id;
+
+    try {
+        // Eliminar la película de la base de datos usando Prisma
+        const deletedMovie = await prismadb.movie.delete({
+            where: {
+                idTMDB: id
+            }
+        });
+
+        console.log('Deleted movie', deletedMovie.title);
+        return new NextResponse("Movie deleted " + deletedMovie.title , { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return new NextResponse("Error" + error, { status: 500 });
+    }
+}
