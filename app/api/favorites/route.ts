96323bc34896a69b381bdf522e05e4043cf2b4eb
tomas <josetomashervas@gmail.com>
2025-07-30 23:09:26 +0200

    enhace favorites routes

--- app/api/favorites/route.ts
+++ app/api/favorites/route.ts
@@ -1,12 +1,36 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import prismadb from "@/lib/prismadb";
+import jwt from 'jsonwebtoken';
 
 export async function GET(request: Request) {
-    const session = await getServerSession(authOptions);
-    if(!session) {
-        return new Response("Unauthorized", {status: 401})
-    }
+     const session = await getServerSession(authOptions);
+        let isAuthenticated = false;
+        let userId: string | undefined;
+        let email: string | undefined;    
+        if (session) {
+            isAuthenticated = true;
+            email = session!.user!.email!
+           
+        } else {
+            const authorizationHeader = request.headers.get('authorization');
+            if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+                const token = authorizationHeader.substring(7);
+                try {
+                    const decoded = jwt.verify(token, process.env.JWT_SECRET! as string) as { userId: string, email: string };
+                    userId = decoded.userId;
+                    email = decoded.email;
+                    isAuthenticated = true;
+                } catch (error) {
+                    console.error("JWT verification failed:", error);
+                    // Token is invalid, proceed to return Unauthorized
+                }
+            }
+        }
+    
+        if (!isAuthenticated) {
+            return new Response("Unauthorized", { status: 401 });
+        }
 
     const { searchParams } = new URL(request.url)
     const profile:string | null = searchParams.get('profile')
@@ -18,7 +42,7 @@ export async function GET(request: Request) {
     try {
         const user = await prismadb.user.findUnique({
             where: {
-                email: session.user!.email!
+                email
             }
         })
         const movies = await prismadb.movie.findMany({
--- app/api/favoritestv/route.ts
+++ app/api/favoritestv/route.ts
@@ -1,12 +1,37 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import prismadb from "@/lib/prismadb";
+import jwt from 'jsonwebtoken';
 
 export async function GET(request: Request) {
-    const session = await getServerSession(authOptions);
-    if(!session) {
-        return new Response("Unauthorized", {status: 401})
-    }
+     const session = await getServerSession(authOptions);
+        let isAuthenticated = false;
+        let userId: string | undefined;
+        let email: string | undefined;    
+    
+        if (session) {
+            isAuthenticated = true;
+            email = session.user!.email!
+           
+        } else {
+            const authorizationHeader = request.headers.get('authorization');
+
+            if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+                const token = authorizationHeader.substring(7);
+                try {
+                    const decoded = jwt.verify(token, process.env.JWT_SECRET! as string) as { userId: string, email: string };
+                    email = decoded.email;
+                    isAuthenticated = true;
+                } catch (error) {
+                    console.error("JWT verification failed:", error);
+                    // Token is invalid, proceed to return Unauthorized
+                }
+            }
+        }
+    
+        if (!isAuthenticated) {
+            return new Response("Unauthorized", { status: 401 });
+        }
 
     const { searchParams } = new URL(request.url)
     const profile:string | null = searchParams.get('profile')
@@ -15,10 +40,10 @@ export async function GET(request: Request) {
         return new Response("Invalid request", {status: 400})
     }
 
-    try {
+    try { 
         const user = await prismadb.user.findUnique({
             where: {
-                email: session.user!.email!
+                email
             }
         })
         const tvs = await prismadb.tVShow.findMany({
