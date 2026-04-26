339ff9ebbadb88b4822d6b04fcad946f8779a6e0
Tomas <josetomashervas@gmail.com>
2026-04-26 11:38:18 +0200

    get seedmovie

--- app/api/seedmovie/[id]/route.ts
+++ app/api/seedmovie/[id]/route.ts
@@ -1,66 +1,103 @@
-import { NextResponse } from 'next/server'
-import prismadb from '@/lib/prismadb'
-import { Movie } from '@prisma/client'
-import { verifyToken } from '@/lib/jwt'
-import { JwtPayload } from 'jsonwebtoken'
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb";
+import { Movie } from "@prisma/client";
+import { verifyToken } from "@/lib/jwt";
+import { JwtPayload } from "jsonwebtoken";
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-    const token = req.headers.get('Authorization')?.split(' ')[1]
-    const decoded = verifyToken(token || '')
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const token = req.headers.get("Authorization")?.split(" ")[1];
+  const decoded = verifyToken(token || "");
 
-    const adminEmails = process.env.ADMIN?.split(',').map(email => email.trim()) || [];
+  const adminEmails =
+    process.env.ADMIN?.split(",").map((email) => email.trim()) || [];
 
-    if (!adminEmails.includes((decoded as JwtPayload).user)) {
-        return new NextResponse('Unauthorized', { status: 403 })
-    }
+  if (!adminEmails.includes((decoded as JwtPayload).user)) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
 
-    const id = +params.id
+  const id = +params.id;
 
-    const movie = await req.json()
+  const movie = await req.json();
 
-    try {
-        const movieDB = await prismadb.movie.update({
-            where: {
-                idTMDB: id
-            },
-            data: {
-                ...movie
-            }
-        })
-        console.log('Stored movie', movieDB.title)
-        return new NextResponse(JSON.stringify(movieDB), { status: 200 })
-
-    } catch (error) {
-        return new NextResponse("Error" + error, { status: 500 })
-    }
+  try {
+    const movieDB = await prismadb.movie.update({
+      where: {
+        idTMDB: id,
+      },
+      data: {
+        ...movie,
+      },
+    });
+    console.log("Stored movie", movieDB.title);
+    return new NextResponse(JSON.stringify(movieDB), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Error" + error, { status: 500 });
+  }
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-    const token = req.headers.get('Authorization')?.split(' ')[1];
-    const decoded = verifyToken(token || '');
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const token = req.headers.get("Authorization")?.split(" ")[1];
+  const decoded = verifyToken(token || "");
 
-    // Verificar si el usuario tiene privilegios de administrador
-    const adminEmails = process.env.ADMIN?.split(',').map(email => email.trim()) || [];
+  // Verificar si el usuario tiene privilegios de administrador
+  const adminEmails =
+    process.env.ADMIN?.split(",").map((email) => email.trim()) || [];
 
-    if (!adminEmails.includes((decoded as JwtPayload).user)) {
-        return new NextResponse('Unauthorized', { status: 403 });
-    }
+  if (!adminEmails.includes((decoded as JwtPayload).user)) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
 
-    const id = +params.id;
+  const id = +params.id;
 
-    try {
-        // Eliminar la película de la base de datos usando Prisma
-        const deletedMovie = await prismadb.movie.delete({
-            where: {
-                idTMDB: id
-            }
-        });
+  try {
+    // Eliminar la película de la base de datos usando Prisma
+    const deletedMovie = await prismadb.movie.delete({
+      where: {
+        idTMDB: id,
+      },
+    });
 
-        console.log('Deleted movie', deletedMovie.title);
-        return new NextResponse("Movie deleted " + deletedMovie.title , { status: 200 });
+    console.log("Deleted movie", deletedMovie.title);
+    return new NextResponse("Movie deleted " + deletedMovie.title, {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Error" + error, { status: 500 });
+  }
+}
 
-    } catch (error) {
-        console.error(error);
-        return new NextResponse("Error" + error, { status: 500 });
-    }
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const token = req.headers.get("Authorization")?.split(" ")[1];
+  const decoded = verifyToken(token || "");
+
+  const adminEmails =
+    process.env.ADMIN?.split(",").map((email) => email.trim()) || [];
+
+  if (!adminEmails.includes((decoded as JwtPayload).user)) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+
+  const id = +params.id;
+
+  const movie = await prismadb.movie.findFirst({
+    where: {
+      idTMDB: id,
+    },
+  });
+
+  if (!movie) {
+    return new NextResponse("Movie not found", { status: 404 });
+  }
+
+  return new NextResponse(JSON.stringify(movie), { status: 200 });
 }
